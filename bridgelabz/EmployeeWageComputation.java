edbb7ee07822c6bbbe61e1fdd19d32d5a8179064
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-04-04 14:03:23 +0530

    Added: Total Wage Queried By Company

--- bridgelabz/EmployeeWageComputation.java
+++ bridgelabz/EmployeeWageComputation.java
@@ -1,16 +1,20 @@
 package com.bridgelabz;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.HashMap;
 public class EmployeeWageComputation implements EmployeeWageBuilder {
     public static final int PART_TIME = 1;          // class constant
     public static final int FULL_TIME = 2;
     private LinkedList<CompanyEmpWage> companyEmpWageList;                        // instance variables
+    private HashMap<String,CompanyEmpWage> companyEmpWageHashMap;
     public EmployeeWageComputation(int n) {                  //Constructor of same class
-        companyEmpWageList = new LinkedList<>();
+        companyEmpWageList = new LinkedList<>();             //using arraylist
+        companyEmpWageHashMap = new HashMap<>();             //using hashmap
     }
     public void addCompany(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
         CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, wagePerHour, maxWorkingDays, maxWorkingHours);
         companyEmpWageList.add(companyEmpWage);
+        companyEmpWageHashMap.put(companyName,companyEmpWage);
     }
     public void computeEmpWage() {
         for (int i = 0; i < companyEmpWageList.size(); i++) {
@@ -19,6 +23,9 @@ public class EmployeeWageComputation implements EmployeeWageBuilder {
             companyEmpWage.storeDailyWage();
             System.out.println(companyEmpWage);
         }
+    }
+    public int getTotalWage(String company){                           //method to get total wage when asked by company
+        return companyEmpWageHashMap.get(company).totalEmpWage;
     }
     public int computeEmpWage(CompanyEmpWage companyEmpWage) {           // Compute Wage method
         int empHrs;                                                      // Local variables
@@ -47,7 +54,7 @@ public class EmployeeWageComputation implements EmployeeWageBuilder {
         }
         return totalEmpHrs * companyEmpWage.WAGE_PER_HR;
     }
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         System.out.println("************************************");
         Scanner scanner = new Scanner(System.in);
@@ -59,6 +66,8 @@ public class EmployeeWageComputation implements EmployeeWageBuilder {
             empWageBuilder.addCompany(scanner.next(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
         }
         empWageBuilder.computeEmpWage();                 // Method calling
+        System.out.println("Enter the company name: ");
+        /*The total wage will be shown after being asked to enter the company name*/
+        System.out.println("The total employee wage is " + empWageBuilder.getTotalWage(scanner.next()));
     }
 }
-
